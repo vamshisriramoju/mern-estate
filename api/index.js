e493baf537cbbdd13463ea828252b25cc78d90e4
vamshisriramoju <vamshitillu34@gmail.com>
2024-10-07 13:30:11 -0500

    connected to mongodb server

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected successfully");
+  })
+  .catch((err) => {
+    console.log("error connecting", err);
+  });
 
 const app = express();
 
