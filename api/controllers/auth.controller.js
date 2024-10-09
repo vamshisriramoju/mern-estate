f6c40b60c1c4f4f1bfefb6262afa710d204ef8fe
vamshisriramoju <vamshitillu34@gmail.com>
2024-10-08 22:06:36 -0500

    created signin route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js"; // Import the User model
 import argon2 from "argon2";
 import { errorHandler } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -29,5 +29,58 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     // Handle any errors during user creation
     return next(error);
+  }
+};
+
+// export const signin = async (req, res, next) => {
+//   const { email, password } = req.body;
+//   try {
+//     const validUser = await User.findOne({ email });
+//     if (!validUser) return next(errorHandler(404, "User not found "));
+
+//     const validPassword = await argon2.verify(validUser.password, password);
+//     if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+
+//     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+//     const {password: pass, ...rest} = validUser._doc;
+//     res
+//       .status(200)
+//       .cookie("access_token", token, {
+//         httpOnly: true,
+//       })
+//       .json(rest);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    console.log("Signin attempt for:", email); // Log the email for debug
+
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      console.log("User not found");
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = await argon2.verify(validUser.password, password);
+    if (!validPassword) {
+      console.log("Wrong password");
+      return next(errorHandler(401, "Wrong Credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    console.log("Generated Token:", token); // Log the generated token
+
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true }) // This sets the cookie in the response
+      .json({ ...rest, accessToken: token }); // Include the token in the JSON response
+  } catch (error) {
+    console.error("Error during signin:", error);
+    next(error);
   }
 };
