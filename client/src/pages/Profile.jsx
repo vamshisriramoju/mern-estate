bf0f6059780e7e611fa2e2055cb5f129877e8fb4
vamshisriramoju <vamshitillu34@gmail.com>
2024-12-09 20:18:56 -0600

    update of profile and made into private

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div>
-      Profile
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
+      <form>
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="'rounded-full h-24 w-24 object-cover 
+cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="name"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+      </form>
     </div>
-  )
+  );
 }
