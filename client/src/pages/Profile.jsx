4d0145ff40c9b2c162f80842a90187096584e3c2
vamshisriramoju <vamshitillu34@gmail.com>
2024-12-24 16:21:23 -0600

    added create listing UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,6 +19,7 @@ import {
   signOutUserFailure,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -181,6 +182,12 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link
+          className=" bg-green-700 text-white rounded-lg p-3 uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create listing
+        </Link>
       </form>
 
       {/* Show upload progress */}
