51ee16e629b0c8f99d80254ea044efd008d814c3
vamshisriramoju <vamshitillu34@gmail.com>
2025-01-03 16:40:41 -0600

    created get user listings API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import argon2 from "argon2";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 export const test = (req, res) => {
   res.json({
     message: "api route is working!!!!",
@@ -42,8 +43,21 @@ export const deleteUser = async (req, res, next) => {
   try {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("acces_token");
-    res.status(200).json("User has been deleted")
+    res.status(200).json("User has been deleted");
   } catch (err) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own user listings"));
   }
 };
