7438ad27241a3887b5a5c0f600e907d93926994f
vamshisriramoju <vamshitillu34@gmail.com>
2025-01-10 22:17:25 -0600

    update listing API and CLIENT

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -51,4 +51,16 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-export default { createListing, deleteListing, updateListing };
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { createListing, deleteListing, updateListing, getListing };
