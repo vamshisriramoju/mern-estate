71f4298b2cea83497be3b0dc9dbc3ec793fc77c4
vamshisriramoju <vamshitillu34@gmail.com>
2025-01-29 12:08:02 -0600

    listing API and Functionality ,contact component

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,7 +3,8 @@ import {
   test,
   updateUser,
   deleteUser,
-  getUserListings
+  getUserListings,
+  getUser
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -13,4 +14,5 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings)
+router.get("/:id", verifyToken, getUser)
 export default router;
